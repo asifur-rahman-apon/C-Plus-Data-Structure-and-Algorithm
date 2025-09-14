136e16441e35bdd19eb2ab0c5057ae883aeaf30e
asifur-rahman-apon <asifapon.drmc@gmail.com>
2025-09-15 00:36:43 +0600

    f

---
+++ Data_Structure/STL_Pair/stl_pair.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    pair<int, int> p;
+    pair<string, int> s;
+    p = make_pair(4, 5);
+    s = {"Apon", 2};
+    cout << p.first << endl;
+    cout << p.second << endl;
+    cout << s.first << endl;
+    cout << s.second << endl;
+
+    return 0;
+}
