ad21bb31eeff11859e450df360fc735cda493620
asifur-rahman-apon <asifapon.drmc@gmail.com>
2025-09-15 13:39:20 +0600

    add

---
+++ Data_Structure/BST/Converting_array_to_BST_implementation.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "N0 TREE" << endl;
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+        cout << f->val << " ";
+        if (f->left != NULL)
+        {
+            q.push(f->left);
+        }
+        if (f->right != NULL)
+        {
+            q.push(f->right);
+        }
+    }
+}
+int main()
+{
+
+    return 0;
+}
